52155c56248606a5a32540e9be84029b9d4360e8
Miles Berry <70924215+Miles-Berry@users.noreply.github.com>
2025-04-13 13:53:54 -0500

    base ui layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,34 @@
 <script>
+	import Separator from '$lib/components/ui/separator/separator.svelte';
 	import '../app.css';
+	import { ModeWatcher } from "mode-watcher";
+
+	import Sun from "@lucide/svelte/icons/sun";
+  	import Moon from "@lucide/svelte/icons/moon";
+	import { toggleMode } from "mode-watcher";
+  	import { Button } from "$lib/components/ui/button/index.js";
+
+	let { children } = $props();
 </script>
 
-<slot></slot>
+<div class="flex text-4xl text-center justify-between p-4 align-middle">
+	<h1>Bike Thief</h1>
+	<Button onclick={toggleMode} variant="outline" size="icon">
+		<Sun
+		  class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+		/>
+		<Moon
+		  class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+		/>
+		<span class="sr-only">Toggle theme</span>
+	</Button>
+</div>
+<div class="px-4 py-0 m-0">
+	<Separator />
+</div>
+<div class="p-4">
+	{@render children?.()}
+</div>
+
+<ModeWatcher />
+
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,29 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+	import { Button } from "$lib/components/ui/button/index";
+	import * as Card from "$lib/components/ui/card/index";
+
+
+</script>
+
+<ul>
+    <li> 
+        <Card.Root>
+            <Card.Header>
+                <Card.Title>
+                    Schuba's Tavern
+                </Card.Title>
+                <Card.Description>
+                    June 11th, 2025
+                </Card.Description>
+            </Card.Header>
+            <Card.Content>
+
+            </Card.Content>
+            <Card.Footer>
+                <Button href="https://google.com">
+                    Tickets
+                </Button>
+            </Card.Footer>
+        </Card.Root>
+    </li>
+</ul>
